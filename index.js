e257427a6cdce39cd3c053317c3fe86d9870d994
kago96 <kago96515@gmail.com>
2026-01-14 21:22:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 export default {
-  async fetch(request) {
-    if (request.method !== "POST") {
-      return new Response("POST only", { status: 405 });
-    }
-
-    let data;
-    try {
-      data = await request.json();
-    } catch {
-      return new Response("Invalid JSON", { status: 400 });
-    }
-
-    return new Response(
-      JSON.stringify({
-        success: true,
-        received: data
-      }),
-      {
-        headers: { "Content-Type": "application/json" }
-      }
-    );
+  async fetch() {
+    return new Response("WORKER OK");
   }
 };
